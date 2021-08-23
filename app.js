7d6038f7bcde893790e0f36f16d243d5e015b9ef
unknown <ravisingh852937@gmil.com>
2021-08-23 12:22:47 +0530

    meanstack

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.get('*', function(req,res) {
 console.log(__dirname+"kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk")
 res.sendFile(path.join(__dirname+'/meanstack/index.html'));
 });
-app.listen(3000, () => {
+app.listen(process.env.PORT||3000, () => {
         console.log("Server start at: " + "http://localhost:" + 3000 + "/")
 
 })
