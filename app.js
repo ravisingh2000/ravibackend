dd94084a844923cd75865debd85750848b8c5800
unknown <ravisingh852937@gmil.com>
2021-08-31 16:14:19 +0530

    add finalchanges

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use("/api", resumeRouter)
 app.use(express.static(__dirname + '/meanstack'));
 app.get("/api/signout", async (req, res) => {
         res.clearCookie("mainproject");
-        res.clearCookie("mainproject1")
+        res.clearCookie("resumeToken")
         res.status(200).json({
                 valid: false
         })
